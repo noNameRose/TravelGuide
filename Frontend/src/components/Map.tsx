2b0142ee329ef275c1469b1b772abae1d9867222
Dat Nguyen <datnguyengithubwork@gmail.com>
2026-08-08 23:49:58 -0500

    fea: set up center and zoom state for map

--- Frontend/src/components/Map.tsx
+++ Frontend/src/components/Map.tsx
@@ -1,13 +1,26 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import mapboxgl from "mapbox-gl";
+
+const INITIAL_CENTER = [
+    -74.0242,
+    40.6941
+];
+
+const INITIAL_ZOOM = 10.12;
+
 const Map = () => {
     const mapRef = useRef<mapboxgl.Map | null>(null);
     const mapContainerRef = useRef<HTMLDivElement | null>(null);
 
+    const [center, setCenter] = useState<[number, number]>(INITIAL_CENTER as [number, number]);
+    const [zoom, setZoom] = useState<number>(INITIAL_ZOOM);
+
     useEffect(() => {
         mapRef.current = new mapboxgl.Map({
             accessToken: import.meta.env.VITE_MAPBOX_API,
-            container: mapContainerRef.current as HTMLDivElement
+            container: mapContainerRef.current as HTMLDivElement,
+            center: center,
+            zoom: zoom
         });
 
         return () => {
