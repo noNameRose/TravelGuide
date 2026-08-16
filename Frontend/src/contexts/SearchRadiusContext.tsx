4bf1670f2f300b1a1e3e5790c728b59d151cb63b
Dat Nguyen <datnguyengithubwork@gmail.com>
2026-08-15 21:05:19 -0500

    fea: add minRadius field to search radius context

--- Frontend/src/contexts/SearchRadiusContext.tsx
+++ Frontend/src/contexts/SearchRadiusContext.tsx
@@ -3,7 +3,8 @@ import { createContext } from "react";
 type SearchChingRadiusType = {
     radius: number,
     handleRadiusChange: (func: ((range: number) => number)) => void,
-    maxRadius: number
+    maxRadius: number,
+    minRadius: number
 };
 
 const SearchRadiusContext = createContext<SearchChingRadiusType | null>(null);
