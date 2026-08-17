b8aae198206e3572115bafadaf663b4f61a55a77
Dat Nguyen <datnguyengithubwork@gmail.com>
2026-08-17 15:07:59 -0500

    style: work on style of spot list

--- Frontend/src/components/DiaryList.tsx
+++ Frontend/src/components/DiaryList.tsx
@@ -4,7 +4,7 @@ import type { SpotList } from "../features/SpotRender/SpotList";
 const DiaryList = ({spotList}: {spotList: SpotList}) => {
     const spots = spotList.getSpotsList();
     return (
-        <div>
+        <div className="flex flex-col">
             {spots.map(spot => (
                 <input
                     value={spot.name}
