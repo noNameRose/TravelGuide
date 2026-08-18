b2a8fdf2ae77a7e119f1a947b50ebca21e935d59
Dat Nguyen <datnguyengithubwork@gmail.com>
2026-08-18 14:50:46 -0500

    chore: remove dummy places in diary list

--- Frontend/src/pages/TravelDiaryPage.tsx
+++ Frontend/src/pages/TravelDiaryPage.tsx
@@ -9,38 +9,38 @@ import IsPlayContext from "../contexts/IsPlayContext";
 
 const INITIAL_LIST = new SpotList();
 
-INITIAL_LIST.addSpot(Spot.builder()
-                        .name("Minneapolis")
-                        .location({
-                            lng: -93.26384,
-                            lat: 44.97997
-                        })
-                        .build()
-);
-INITIAL_LIST.addSpot(Spot.builder()
-                        .name("Chicago")
-                        .location({
-                            lng: -87.6298,
-                            lat: 41.8781
-                        })
-                        .build()
-);
-INITIAL_LIST.addSpot(Spot.builder()
-                        .name("Las Vegas")
-                        .location({
-                            lng: -115.137,
-                            lat: 36.175
-                        })
-                        .build()
-);
-INITIAL_LIST.addSpot(Spot.builder()
-                        .name("Incline Village")
-                        .location({
-                            lng: -119.9730,
-                            lat: 39.2513
-                        })
-                        .build()
-);
+// INITIAL_LIST.addSpot(Spot.builder()
+//                         .name("Minneapolis")
+//                         .location({
+//                             lng: -93.26384,
+//                             lat: 44.97997
+//                         })
+//                         .build()
+// );
+// INITIAL_LIST.addSpot(Spot.builder()
+//                         .name("Chicago")
+//                         .location({
+//                             lng: -87.6298,
+//                             lat: 41.8781
+//                         })
+//                         .build()
+// );
+// INITIAL_LIST.addSpot(Spot.builder()
+//                         .name("Las Vegas")
+//                         .location({
+//                             lng: -115.137,
+//                             lat: 36.175
+//                         })
+//                         .build()
+// );
+// INITIAL_LIST.addSpot(Spot.builder()
+//                         .name("Incline Village")
+//                         .location({
+//                             lng: -119.9730,
+//                             lat: 39.2513
+//                         })
+//                         .build()
+// );
 INITIAL_LIST.addSpot(Spot.builder()
                         .name("Hanoi")
                         .location({
