764e20e4e0e5d254c5e319c5ed144b05f44d2462
Dat Nguyen <datnguyengithubwork@gmail.com>
2026-08-20 16:27:54 -0500

    fea: configure anime's speed

--- Frontend/src/components/TravelMap.tsx
+++ Frontend/src/components/TravelMap.tsx
@@ -332,7 +332,7 @@ const TravelMap = ({spotList}: {spotList: SpotList}) => {
                 })
                 .to(progress, {
                     t: 1,
-                    duration: 100,
+                    duration: totalLength/20,
                     onUpdate: () => {
                         animatedLine.geometry.coordinates = (routes as [number, number][]).slice(0, progress.t * (routes?.length as number) + 1) ;
                         const source = mapRef.current?.getSource(`driving-${i}`) as mapboxgl.GeoJSONSource;
