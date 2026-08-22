490498e95f4a9fc467c8286fd2cdf1e4799227ea
Dat Nguyen <datnguyengithubwork@gmail.com>
2026-08-21 22:51:24 -0500

    fea: Set up initial security configuration

---
+++ Backend/src/main/java/com/backend/TravelDiary/configs/SecurityConfiguration.java
@@ -0,0 +1,25 @@
+package com.backend.TravelDiary.configs;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfiguration {
+
+  @Bean
+  public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+    http.csrf(c -> c.disable())
+        .authorizeHttpRequests(
+            c -> c.requestMatchers("/*").permitAll()
+        );
+
+    return http.build();
+  }
+}
