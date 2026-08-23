087a58cffe31a6664f4a1e25a524c455bc61700d
Dat Nguyen <datnguyengithubwork@gmail.com>
2026-08-23 17:17:40 -0500

    fea: Set up Login page with google login button

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import { Route, Routes } from "react-router-dom";
 import LandingPage from "./pages/LandingPage";
 import TravelDiaryPage from "./pages/TravelDiaryPage";
+import LoginPage from "./pages/LoginPage";
 
 
 const App = () => {
@@ -9,6 +10,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<LandingPage/>}/>
         <Route path="/travel-diary" element={<TravelDiaryPage/>}/>
+        <Route path="/login" element={<LoginPage/>}/>
       </Routes>
     </>
 
