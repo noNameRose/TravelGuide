3f11d8272d56dad55555e3d4c7018fe1b642228a
Dat Nguyen <datnguyengithubwork@gmail.com>
2026-08-23 20:23:18 -0500

    fea: Set up user model

---
+++ Backend/src/main/java/com/backend/TravelDiary/models/User.java
@@ -0,0 +1,69 @@
+package com.backend.TravelDiary.models;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.sql.Time;
+import java.sql.Timestamp;
+import java.util.Collection;
+import java.util.List;
+
+
+@Data
+@Table(name = "users")
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class User implements UserDetails {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+  private String userId;
+  private String name;
+  @Column(unique = true)
+  private String email;
+  private String password;
+  private boolean isAccountVerified;
+  @CreationTimestamp
+  @Column(updatable = false)
+  private Timestamp createAt;
+  @UpdateTimestamp
+  private Timestamp updateAt;
+
+  @Override
+  public Collection<? extends GrantedAuthority> getAuthorities() {
+    return List.of();
+  }
+
+  @Override
+  public String getUsername() {
+    return this.email;
+  }
+
+  @Override
+  public boolean isAccountNonExpired() {
+    return true;
+  }
+
+  @Override
+  public boolean isAccountNonLocked() {
+    return true;
+  }
+
+  @Override
+  public boolean isCredentialsNonExpired() {
+    return true;
+  }
+
+  @Override
+  public boolean isEnabled() {
+    return true;
+  }
+}
