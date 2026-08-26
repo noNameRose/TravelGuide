104a075aba78d35ffe2b6df509f505654d626504
Dat Nguyen <datnguyengithubwork@gmail.com>
2026-08-26 18:12:51 -0500

    fea: Update Route with explore page

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import LandingPage from "./pages/LandingPage";
 import TravelDiaryPage from "./pages/TravelDiaryPage";
 import LoginPage from "./pages/LoginPage";
+import ExplorePage from "./pages/ExplorePage";
 
 
 const App = () => {
@@ -9,6 +10,7 @@ const App = () => {
     <>
       <Routes>
         <Route path="/" element={<LandingPage/>}/>
+        <Route path="/explore" element={<ExplorePage/>}/>
         <Route path="/travel-diary" element={<TravelDiaryPage/>}/>
         <Route path="/login" element={<LoginPage/>}/>
       </Routes>
