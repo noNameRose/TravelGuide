70155152f1b3492ca827ebe9cbadcc9d12227bdd
Dat Nguyen <datnguyengithubwork@gmail.com>
2026-08-27 14:05:20 -0500

    fea: wrap the whole app inside NavigationController component

--- Frontend/src/main.tsx
+++ Frontend/src/main.tsx
@@ -3,11 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { BrowserRouter } from 'react-router-dom'
+import NavigationController from './components/nav/NavigationController.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
     <StrictMode>
-      <App />
+      <NavigationController>
+        <App />
+      </NavigationController>
     </StrictMode>,
   </BrowserRouter>
 )
