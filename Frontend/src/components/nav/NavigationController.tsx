70bf06f0c433bee30dae452955e7612dba0427c0
Dat Nguyen <datnguyengithubwork@gmail.com>
2026-08-27 14:02:50 -0500

    fea: Set up NavigationController component

---
+++ Frontend/src/components/nav/NavigationController.tsx
@@ -0,0 +1,20 @@
+import type { ReactNode } from "react";
+import NavigationBar from "./NavigationBar";
+import { useLocation } from "react-router-dom";
+
+type NavigationControllerProp = {
+    children: ReactNode
+}
+
+const NavigationController = ({children}: NavigationControllerProp) => {
+    const location = useLocation();
+    const showNavigationBar = location.pathname;
+    return (
+        <div>
+            <NavigationBar/>
+            {children}
+        </div>
+    );
+};
+
+export default NavigationController;
