1f1c3feb19f646274140de0c308bf3ede64ec2da
Dat Nguyen <datnguyengithubwork@gmail.com>
2026-08-27 17:19:08 -0500

    fea: Set up function to get place's photo url

---
+++ Frontend/src/utils/getPlacePhotoUri.tsx
@@ -0,0 +1,27 @@
+import searchCoordinate, { type GeoCodingResBody } from "./searchCoordinate";
+
+type PhotoResponseBody = {
+    photos: Photo[]
+}
+
+type Photo = {
+    name: string,
+    width: number, 
+    height: number
+};
+
+const getPlacePhotoUri = async (placeName: string) => {
+    const geocodeResponse = (await searchCoordinate(placeName)) as GeoCodingResBody;
+    const placeId = geocodeResponse.results[0].place_id;
+    const response = await fetch(import.meta.env.VITE_PLACE_DETAILS_URL + placeId, {
+        headers: {
+            "Content-Type": "application/json",
+            "X-Goog-Api-Key": import.meta.env.VITE_PLACE_API_KEY,
+            "X-Goog-FieldMask": "photos"
+        }
+    });
+    const body = (await response.json()) as PhotoResponseBody;
+    return import.meta.env.VITE_PLACE_PICTURE_API_URL + body.photos[0].name + `/media?key=${import.meta.env.VITE_PLACE_API_KEY}&maxHeightPx=${body.photos[0].height}&maxWidthPx=${body.photos[0].width}`;
+};
+
+export default getPlacePhotoUri;
