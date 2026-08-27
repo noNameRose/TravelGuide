b6257f628b7e52ed24c5e73f08ac85cbbaf51850
Dat Nguyen <datnguyengithubwork@gmail.com>
2026-08-27 15:03:05 -0500

    fea: Wrapp the svg icon inside a dive

--- Frontend/src/components/nav/NavIcon.tsx
+++ Frontend/src/components/nav/NavIcon.tsx
@@ -1,4 +1,4 @@
-type IconName = "profile" | "explore" | "dirary";
+export type IconName = "profile" | "explore" | "diary";
 
 type NavIconType = {
     name: IconName,
@@ -8,12 +8,13 @@ type NavIconType = {
 
 const NavIcon = ({name, path, className}: NavIconType) => {
     let icon = null;
+    const svgClass = "w-full h-full";
     if (name === "profile") {
         icon = (
             <svg 
                 xmlns="http://www.w3.org/2000/svg" 
                 viewBox="0 0 640 640"
-                className={className}
+                className={svgClass}
             >
                 <path 
                     fill="#ecf7f9" 
@@ -26,26 +27,30 @@ const NavIcon = ({name, path, className}: NavIconType) => {
             <svg 
                 xmlns="http://www.w3.org/2000/svg" 
                 viewBox="0 0 640 640"
-                className={className}
+                className={svgClass}
             >
                 <path fill="#ecf7f9" d="M480 272C480 317.9 465.1 360.3 440 394.7L566.6 521.4C579.1 533.9 579.1 554.2 566.6 566.7C554.1 579.2 533.8 579.2 521.3 566.7L394.7 440C360.3 465.1 317.9 480 272 480C157.1 480 64 386.9 64 272C64 157.1 157.1 64 272 64C386.9 64 480 157.1 480 272zM272 416C351.5 416 416 351.5 416 272C416 192.5 351.5 128 272 128C192.5 128 128 192.5 128 272C128 351.5 192.5 416 272 416z"/>
             </svg>
         );
     }
-    if (name === "dirary") {
+    if (name === "diary") {
         icon = (
             <svg 
                 xmlns="http://www.w3.org/2000/svg" 
                 viewBox="0 0 24 24"
-                className={className}
+                className={svgClass}
             >
-                <path d="M8 2V22H4V18H2V16H4V13H2V11H4V8H2V6H4V2H8ZM20.0049 2C21.1068 2 22 2.89821 22 3.9908V20.0092C22 21.1087 21.1074 22 20.0049 22H10V2H20.0049Z">
+                <path 
+                    fill="red"
+                    d="M8 2V22H4V18H2V16H4V13H2V11H4V8H2V6H4V2H8ZM20.0049 2C21.1068 2 22 2.89821 22 3.9908V20.0092C22 21.1087 21.1074 22 20.0049 22H10V2H20.0049Z">
                 </path>
             </svg>
         );
     }
     return (
-        {icon}
+        <div className={className}>
+            {icon}
+        </div>
     );
 };
 
