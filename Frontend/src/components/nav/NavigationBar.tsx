329c1e64616841516e041e7744dcebaf99421f87
Dat Nguyen <datnguyengithubwork@gmail.com>
2026-08-27 14:49:21 -0500

    fea: Set up list of navigation

--- Frontend/src/components/nav/NavigationBar.tsx
+++ Frontend/src/components/nav/NavigationBar.tsx
@@ -1,7 +1,23 @@
+const navList = [
+    {
+        name: "profile",
+        path: "/profile"
+    },
+    {
+        name: "explore",
+        path: "/explore"
+    },
+    {
+        name: "diary",
+        path: "/diary"
+    }
+];
+
 const NavigationBar = () => {
     return (
-        <>
-        </>
+        <div className="w-[10vw] min-h-screen bg-blue_200">
+            
+        </div>
     );
 };
 
