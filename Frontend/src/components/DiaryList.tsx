abe58baf4c39be29212d1c01139011bcb954fc23
Dat Nguyen <datnguyengithubwork@gmail.com>
2026-08-27 18:05:56 -0500

    style: works on style of diary list

--- Frontend/src/components/DiaryList.tsx
+++ Frontend/src/components/DiaryList.tsx
@@ -5,7 +5,7 @@ import SpotComponent from "./SpotComponent";
 const DiaryList = ({spotList}: {spotList: SpotList}) => {
     const spots = spotList.getSpotsList();
     return (
-        <div className="flex flex-col gap-4">
+        <div className="flex flex-col gap-4 items-center">
             {spots.map(spot => (
                 <SpotComponent
                     name={spot.name}
