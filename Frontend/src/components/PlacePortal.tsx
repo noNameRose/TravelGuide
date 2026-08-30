a0020d10eb9a5b9d6f6dcd82992c44b8446fc688
Dat Nguyen <datnguyengithubwork@gmail.com>
2026-08-29 23:12:55 -0500

    fea: Add x-mark button to reset the query input

--- Frontend/src/components/PlacePortal.tsx
+++ Frontend/src/components/PlacePortal.tsx
@@ -91,11 +91,21 @@ const PlacePortal = ({isShow, handleShow}: PlacePortalType) => {
                         </svg>
                         <p>What is your next destination?</p>
                     </label>
-                    <input
-                        value={query}
-                        onChange={(e) => setQuery(e.target.value)}
-                        className="bg-blue_950 text-[1rem] p-[0.3em] rounded-[.5em] text-white"
-                    />
+                    <div className="self-stretch relative">
+                        <input
+                            value={query}
+                            onChange={(e) => setQuery(e.target.value)}
+                            className="bg-blue_950 text-[1rem] p-[0.5em] rounded-[.5em] text-white w-full"
+                        />
+                        <svg 
+                            xmlns="http://www.w3.org/2000/svg" 
+                            viewBox="0 0 640 640"
+                            className="w-[2rem] absolute top-1/2 -translate-y-1/2 right-[.5em] cursor-pointer"
+                            onClick={() => setQuery("")}
+                        >
+                            <path fill="#ecf7f9" d="M320 576C461.4 576 576 461.4 576 320C576 178.6 461.4 64 320 64C178.6 64 64 178.6 64 320C64 461.4 178.6 576 320 576zM231 231C240.4 221.6 255.6 221.6 264.9 231L319.9 286L374.9 231C384.3 221.6 399.5 221.6 408.8 231C418.1 240.4 418.2 255.6 408.8 264.9L353.8 319.9L408.8 374.9C418.2 384.3 418.2 399.5 408.8 408.8C399.4 418.1 384.2 418.2 374.9 408.8L319.9 353.8L264.9 408.8C255.5 418.2 240.3 418.2 231 408.8C221.7 399.4 221.6 384.2 231 374.9L286 319.9L231 264.9C221.6 255.5 221.6 240.3 231 231z"/>
+                        </svg>
+                    </div>
                 </div>
                 <div className="flex flex-col gap-4">
                     <label className="flex gap-4 items-center text-[1.5rem] font-bold">
