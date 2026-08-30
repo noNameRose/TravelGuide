4cc0b623615d5f8fe2bbdcaf97035d34ef84ea0c
Dat Nguyen <datnguyengithubwork@gmail.com>
2026-08-29 23:25:56 -0500

    fea: Add route for edit-diary page

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -3,6 +3,7 @@ import LandingPage from "./pages/LandingPage";
 import TravelDiaryPage from "./pages/TravelDiaryPage";
 import LoginPage from "./pages/LoginPage";
 import ExplorePage from "./pages/ExplorePage";
+import EditDiaryPage from "./pages/EditDiaryPage";
 
 
 const App = () => {
@@ -12,6 +13,7 @@ const App = () => {
         <Route path="/" element={<LandingPage/>}/>
         <Route path="/explore" element={<ExplorePage/>}/>
         <Route path="/travel-diary" element={<TravelDiaryPage/>}/>
+        <Route path="/edit" element={<EditDiaryPage/>}/>
         <Route path="/login" element={<LoginPage/>}/>
       </Routes>
     </>
