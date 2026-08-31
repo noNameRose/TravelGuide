3e22fcbcd9be329e6663d5ad3e5417da521647ce
Dat Nguyen <datnguyengithubwork@gmail.com>
2026-08-31 16:45:20 -0500

    fea: create userId for user

--- Backend/src/main/java/com/backend/TravelDiary/services/impl/UserServiceImpl.java
+++ Backend/src/main/java/com/backend/TravelDiary/services/impl/UserServiceImpl.java
@@ -6,6 +6,8 @@ import com.backend.TravelDiary.services.UserService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import java.util.UUID;
+
 @Service
 public class UserServiceImpl implements UserService {
 
@@ -23,6 +25,7 @@ public class UserServiceImpl implements UserService {
     User newUser = User.builder()
         .email(email)
         .name(name)
+        .userId(UUID.randomUUID().toString())
         .isAccountVerified(isAccountVerified)
         .build();
     this.userRepo.save(newUser);
