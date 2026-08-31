20a3ebd570cc1dabf6a7d1d12cfde67830a9edb7
Dat Nguyen <datnguyengithubwork@gmail.com>
2026-08-30 21:18:11 -0500

    fea: Add function to check if user exist in the database

--- Backend/src/main/java/com/backend/TravelDiary/repos/UserRepo.java
+++ Backend/src/main/java/com/backend/TravelDiary/repos/UserRepo.java
@@ -10,4 +10,5 @@ import java.util.Optional;
 @Repository
 public interface UserRepo extends JpaRepository<User, Long> {
   Optional<User> findByEmail(String email);
+  boolean existsByEmail(String email);
 }
