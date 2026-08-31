3ffa6a060ed3046feacf99a0e44a5dabcde808ef
Dat Nguyen <datnguyengithubwork@gmail.com>
2026-08-30 21:04:28 -0500

    fea: Add function to find user by email

--- Backend/src/main/java/com/backend/TravelDiary/repos/UserRepo.java
+++ Backend/src/main/java/com/backend/TravelDiary/repos/UserRepo.java
@@ -5,7 +5,9 @@ import com.backend.TravelDiary.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepo extends JpaRepository<User, Long> {
-
+  Optional<User> findByEmail(String email);
 }
