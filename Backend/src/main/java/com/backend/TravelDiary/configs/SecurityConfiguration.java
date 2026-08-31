a38c994ce9a397619eae95c16601f35a2d000f42
Dat Nguyen <datnguyengithubwork@gmail.com>
2026-08-30 20:40:05 -0500

    fea: add jwt filter to the filter chain

--- Backend/src/main/java/com/backend/TravelDiary/configs/SecurityConfiguration.java
+++ Backend/src/main/java/com/backend/TravelDiary/configs/SecurityConfiguration.java
@@ -1,16 +1,20 @@
 package com.backend.TravelDiary.configs;
 
+import com.backend.TravelDiary.filters.JwtFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfiguration {
+
+  private final JwtFilter jwtFilter;
 
   @Bean
   public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -18,7 +22,8 @@ public class SecurityConfiguration {
     http.csrf(c -> c.disable())
         .authorizeHttpRequests(
             c -> c.requestMatchers("/*", "/auth/google/login", "/auth/google/callback").permitAll()
-        );
+        ).addFilterBefore(this.jwtFilter, UsernamePasswordAuthenticationFilter.class)
+        .logout(c -> c.disable());
 
     return http.build();
   }
