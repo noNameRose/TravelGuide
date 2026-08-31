d93215d0a75889e2922f8be2bb00669dfd96d794
Dat Nguyen <datnguyengithubwork@gmail.com>
2026-08-31 16:11:34 -0500

    fea: Add functionaility to save refresh token in the database

--- Backend/src/main/java/com/backend/TravelDiary/services/RefreshTokenService.java
+++ Backend/src/main/java/com/backend/TravelDiary/services/RefreshTokenService.java
@@ -1,7 +1,10 @@
 package com.backend.TravelDiary.services;
 
+import com.backend.TravelDiary.models.User;
+
 public interface RefreshTokenService {
   public String hashToken(String token);
   public String generateToken();
   public String validateToken(String token);
+  public void saveToken(String token, User user);
 }
--- Backend/src/main/java/com/backend/TravelDiary/services/impl/RefreshTokenServiceImpl.java
+++ Backend/src/main/java/com/backend/TravelDiary/services/impl/RefreshTokenServiceImpl.java
@@ -1,8 +1,11 @@
 package com.backend.TravelDiary.services.impl;
 
+import com.backend.TravelDiary.models.RefreshToken;
+import com.backend.TravelDiary.models.User;
 import com.backend.TravelDiary.repos.RefreshTokenRepo;
 import com.backend.TravelDiary.services.RefreshTokenService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.security.SecureRandom;
@@ -11,6 +14,9 @@ import java.util.UUID;
 
 @Service
 public class RefreshTokenServiceImpl implements RefreshTokenService {
+
+  @Value(value = "${jwt.time.expires}")
+  private Long REFRESH_TOKEN_EXPIRES_IN;
 
   @Autowired
   private final RefreshTokenRepo refreshTokenRepo;
@@ -31,9 +37,22 @@ public class RefreshTokenServiceImpl implements RefreshTokenService {
     return UUID.randomUUID().toString();
   }
 
+
+
   @Override
   public String validateToken(String token) {
 
     return "";
+  }
+
+  @Override
+  public void saveToken(String token, User user) {
+    RefreshToken newToken = RefreshToken
+        .builder()
+        .hashedToken(this.hashToken(token))
+        .user(user)
+        .expiresAt(System.currentTimeMillis() + this.REFRESH_TOKEN_EXPIRES_IN)
+        .build();
+    this.refreshTokenRepo.save(newToken);
   }
 }
