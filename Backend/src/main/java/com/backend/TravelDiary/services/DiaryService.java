89baa49868f086e471685a5b44d2e3fca330eb7a
Dat Nguyen <datnguyengithubwork@gmail.com>
2026-09-01 11:46:13 -0500

    fea: Set up diary service

---
+++ Backend/src/main/java/com/backend/TravelDiary/services/DiaryService.java
@@ -0,0 +1,13 @@
+package com.backend.TravelDiary.services;
+
+import com.backend.TravelDiary.dto.DiaryResponse;
+import com.backend.TravelDiary.models.Diary;
+import com.backend.TravelDiary.models.User;
+
+import java.util.List;
+
+public interface DiaryService {
+  public List<Diary> findDiariesByUser(User user);
+  public Diary createDiary(String name, User user);
+  public List<DiaryResponse> convertToDiaryResponse(List<Diary> diaries);
+}
---
+++ Backend/src/main/java/com/backend/TravelDiary/services/impl/DiaryServiceImp.java
@@ -0,0 +1,53 @@
+package com.backend.TravelDiary.services.impl;
+
+
+import com.backend.TravelDiary.dto.DiariesResponse;
+import com.backend.TravelDiary.dto.DiaryResponse;
+import com.backend.TravelDiary.models.Diary;
+import com.backend.TravelDiary.models.User;
+import com.backend.TravelDiary.repos.DiaryRepo;
+import com.backend.TravelDiary.services.DiaryService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class DiaryServiceImp implements DiaryService {
+
+  private final DiaryRepo diaryRepo;
+
+  @Override
+  public List<Diary> findDiariesByUser(User user) {
+    List<Diary> diaries = this.diaryRepo.findByUser(user).orElseThrow(() -> new RuntimeException());
+    return diaries;
+  }
+
+  public Diary createDiary(String name, User user) {
+    Diary newDiary = Diary
+        .builder()
+        .name(name)
+        .diaryId(UUID.randomUUID().toString())
+        .user(user)
+        .build();
+    this.diaryRepo.save(newDiary);
+    return newDiary;
+  }
+
+  @Override
+  public List<DiaryResponse> convertToDiaryResponse(List<Diary> diaries) {
+    List<DiaryResponse> responses = new ArrayList<>();
+    for (Diary diary: diaries) {
+      responses.add(DiaryResponse
+          .builder()
+          .name(diary.getName())
+          .diaryId(diary.getDiaryId())
+          .build()
+      );
+    }
+    return responses;
+  }
+}
