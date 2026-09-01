7ab8c49d130b820a3e600c75f47650b2b853a592
Dat Nguyen <datnguyengithubwork@gmail.com>
2026-08-31 19:27:55 -0500

    fea: Set up user controller

---
+++ Backend/src/main/java/com/backend/TravelDiary/controllers/UserController.java
@@ -0,0 +1,33 @@
+package com.backend.TravelDiary.controllers;
+
+import com.backend.TravelDiary.dto.UserResponse;
+import com.backend.TravelDiary.models.User;
+import com.backend.TravelDiary.services.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.CurrentSecurityContext;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class UserController {
+
+  private final UserService userService;
+
+
+  @GetMapping("/user")
+  public ResponseEntity<UserResponse> getUserProfile(
+      @CurrentSecurityContext(expression = "authentication?.name") String email
+  ) {
+    User user = (User) this.userService.loadUserByUsername(email);
+    return ResponseEntity.ok(
+        UserResponse
+            .builder()
+            .name(user.getName())
+            .email(user.getEmail())
+            .build()
+    );
+  }
+
+}
