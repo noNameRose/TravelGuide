3521d60d55bde3a184168ed2dae6fafda464ad0b
Dat Nguyen <datnguyengithubwork@gmail.com>
2026-08-31 20:37:06 -0500

    fea: send empty header when access token is null

--- Frontend/src/utils/getUserProfile.tsx
+++ Frontend/src/utils/getUserProfile.tsx
@@ -1,17 +1,30 @@
+import getAccessToken from "./getAccessToken";
+
 type UserResBody = {
     email: string,
     name: string
+};
+
+type HeaderType = {
+    "Authorization": string
 }
 
-const getUserProfile = async (accessToken: string) => {
+const getUserProfile = async (accessToken: string | null) => {
+    let headers: HeaderType | {} = {
+        "Authorization": `Bearer ${accessToken}`
+    };
+    if (accessToken === null) {
+        headers = {};
+    }
     const response = await fetch(import.meta.env.VITE_SERVER_ORIGIN + "/user", {
-        headers: {
-            "Authorization": `Bearer ${accessToken}`
-        }
+        headers: headers
     });
     if (response.ok) {
         const body = (await response.json()) as UserResBody;
         return body;
+    }
+    else {
+        throw new Error("Something went wrong");
     }
 };
 
