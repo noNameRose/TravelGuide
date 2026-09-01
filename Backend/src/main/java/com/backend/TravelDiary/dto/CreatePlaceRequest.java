2db06e58eb8399f58bbb6911e57d5d45c6a62c36
Dat Nguyen <datnguyengithubwork@gmail.com>
2026-09-01 18:26:37 -0500

    fea: Add dto for new place request

---
+++ Backend/src/main/java/com/backend/TravelDiary/dto/CreatePlaceRequest.java
@@ -0,0 +1,18 @@
+package com.backend.TravelDiary.dto;
+
+import lombok.*;
+
+@Data
+@Builder
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class CreatePlaceRequest {
+  private String name;
+  private String googlePlaceId;
+  private int index;
+  private double lng;
+  private double lat;
+  private String getHereBy;
+}
