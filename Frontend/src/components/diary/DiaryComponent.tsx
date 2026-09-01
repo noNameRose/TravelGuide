4fd123a428ec883958cb71d267307726b8ed3165
Dat Nguyen <datnguyengithubwork@gmail.com>
2026-09-01 12:34:41 -0500

    fea: work on UI of diary component

--- Frontend/src/components/diary/DiaryComponent.tsx
+++ Frontend/src/components/diary/DiaryComponent.tsx
@@ -1,4 +1,5 @@
 import type { Diary } from "../../utils/getDiaries";
+import diaryImage from "../../assets/mapimage.png";
 
 type DiaryComponentType = {
     diary: Diary
@@ -6,8 +7,23 @@ type DiaryComponentType = {
 
 const DiaryComponent = ({diary}: DiaryComponentType) => {
     return (
-        <div>
-            <p>{diary.name}</p>
+        <div className="flex gap-4">
+            <div
+                className="rounded-[1em]"
+                style={
+                    {
+                        width: "15rem",
+                        height: "10rem",
+                        backgroundImage: `url(${diaryImage})`,
+                        backgroundSize: "cover",
+                        backgroundPosition: "center"
+                        
+                    }
+                }
+            ></div>
+            <div>
+                <p className="font-bold text-[1.5rem] text-blue_400">{diary.name}</p>
+            </div>
         </div>
     );
 };
