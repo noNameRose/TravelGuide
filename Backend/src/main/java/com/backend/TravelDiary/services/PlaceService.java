80da593124c0f7aae334c1ccfb7631b2af5fb850
Dat Nguyen <datnguyengithubwork@gmail.com>
2026-09-02 14:45:39 -0500

    fea: Add method to get places in order and place response

--- Backend/src/main/java/com/backend/TravelDiary/services/PlaceService.java
+++ Backend/src/main/java/com/backend/TravelDiary/services/PlaceService.java
@@ -2,9 +2,14 @@ package com.backend.TravelDiary.services;
 
 
 import com.backend.TravelDiary.dto.CreatePlaceRequest;
+import com.backend.TravelDiary.dto.PlaceResponse;
 import com.backend.TravelDiary.models.Diary;
 import com.backend.TravelDiary.models.Place;
 
+import java.util.List;
+
 public interface PlaceService {
   public Place createPlace(CreatePlaceRequest request, Diary diary);
+  public List<Place> getPlacesInOrder(String diaryId);
+  public List<PlaceResponse> getPlaceResponse(String diaryId);
 }
