445c42bfdede7568b83c6f8125896f04c618f61c
Dat Nguyen <datnguyengithubwork@gmail.com>
2026-09-02 14:53:26 -0500

    fea: work on animation when user hover diary

--- Frontend/src/components/diary/DiaryComponent.tsx
+++ Frontend/src/components/diary/DiaryComponent.tsx
@@ -1,13 +1,52 @@
 import type { Diary } from "../../utils/getDiaries";
 import diaryImage from "../../assets/mapimage.png";
+import { useEffect, useRef, useState } from "react";
+import gsap from "gsap";
+import { useNavigate } from "react-router-dom";
 
 type DiaryComponentType = {
     diary: Diary
 }
 
 const DiaryComponent = ({diary}: DiaryComponentType) => {
+    const container = useRef<HTMLDivElement | null>(null);
+    const name = useRef<HTMLParagraphElement | null>(null);
+    const [isHover, setIsHover] = useState(false);
+    const tl = useRef<GSAPTimeline | null>(null);
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        tl.current = gsap.timeline({defaults: {ease: "power4"}});
+        if (isHover) {
+            tl.current
+            .to(container.current, {
+                backgroundColor: "#64BECE"
+            })
+            .to(name.current, {
+                color:  "#ECF7F9"
+            }, "<")
+
+        }
+        else {
+            tl.current
+            .to(container.current, {
+                backgroundColor: "#ECF7F9"
+            })
+            .to(name.current, {
+                color: "#64BECE"
+            }, "<")
+            
+        }
+    }, [isHover]);
+    
     return (
-        <div className="flex gap-4">
+        <div 
+            className="flex gap-4 p-[.5em] rounded-[1.5em] cursor-pointer"
+            ref={container}
+            onMouseOver={() => setIsHover(true)}
+            onMouseOut={() => setIsHover(false)}
+            onClick={() => navigate(`/diary/edit?id=${diary.diaryId}`)}
+        >
             <div
                 className="rounded-[1em]"
                 style={
@@ -22,7 +61,10 @@ const DiaryComponent = ({diary}: DiaryComponentType) => {
                 }
             ></div>
             <div>
-                <p className="font-bold text-[1.5rem] text-blue_400">{diary.name}</p>
+                <p 
+                    className="font-bold text-[1.5rem] text-blue_400"
+                    ref={name}
+                >{diary.name}</p>
             </div>
         </div>
     );
