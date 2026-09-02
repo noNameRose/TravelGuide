50d2a865955a4518620c801c97776d561ef119d5
Dat Nguyen <datnguyengithubwork@gmail.com>
2026-09-01 20:30:15 -0500

    fea: Use size of current diary as neext index

--- Backend/src/main/java/com/backend/TravelDiary/services/impl/PlaceServiceImp.java
+++ Backend/src/main/java/com/backend/TravelDiary/services/impl/PlaceServiceImp.java
@@ -1,12 +1,15 @@
 package com.backend.TravelDiary.services.impl;
 
 import com.backend.TravelDiary.dto.CreatePlaceRequest;
+import com.backend.TravelDiary.exceptions.DiaryNotFoundException;
 import com.backend.TravelDiary.models.Diary;
 import com.backend.TravelDiary.models.Place;
 import com.backend.TravelDiary.repos.PlaceRepo;
 import com.backend.TravelDiary.services.PlaceService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -16,6 +19,7 @@ public class PlaceServiceImp implements PlaceService {
 
   @Override
   public Place createPlace(CreatePlaceRequest request, Diary diary) {
+    List<Place> places = placeRepo.findByDiary(diary).orElseThrow(() -> new DiaryNotFoundException("Places are not found"));
     Place newPlace = Place
         .builder()
         .name(request.getName())
@@ -24,7 +28,7 @@ public class PlaceServiceImp implements PlaceService {
         .lng(request.getLng())
         .diary(diary)
         .getHereBy(request.getGetHereBy())
-        .index(request.getIndex())
+        .index(places.size())
         .build();
     this.placeRepo.save(newPlace);
     return newPlace;
