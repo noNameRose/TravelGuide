3f68061fd7b4b9413d02658e6bbae511678b0c23
Dat Nguyen <datnguyengithubwork@gmail.com>
2026-09-02 15:00:47 -0500

    fea: set up transporation constant from front-end

---
+++ Backend/src/main/java/com/backend/TravelDiary/models/Transportation.java
@@ -0,0 +1,7 @@
+package com.backend.TravelDiary.models;
+
+public final class Transportation {
+  public static final String CAR = "driving";
+  public static final String FLY = "flying";
+  public static final String WALKING = "walking";
+}
