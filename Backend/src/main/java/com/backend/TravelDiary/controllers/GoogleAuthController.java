e258b208c5b96fd07e997031254116416a36a33d
noNameRose <datnguyengithubwork@gmail.com>
2026-09-03 07:08:37 +0700

    fea: change cookie setting to lax

--- Backend/src/main/java/com/backend/TravelDiary/controllers/GoogleAuthController.java
+++ Backend/src/main/java/com/backend/TravelDiary/controllers/GoogleAuthController.java
@@ -140,7 +140,7 @@ public class GoogleAuthController {
         .from("refreshToken", refreshToken)
         .httpOnly(true)
         .secure(true)
-        .sameSite("None")
+        .sameSite("Lax")
         .maxAge(Duration.ofDays(7))
         .path("/")
         .build();
